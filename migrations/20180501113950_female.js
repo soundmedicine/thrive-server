3c22396d8142da9e3f126032147a0cfab28293a6
Roberto Santos <robertosantos@Robertos-MacBook-Pro.local>
2018-05-05 21:55:29 -0600

    update deployed link

--- migrations/20180501113950_female.js
+++ migrations/20180501113950_female.js
@@ -1,22 +1,22 @@
 exports.up = function(knex, Promise) {
-    return knex.schema.createTable('female', table => {
-        table.increments().primary()
-        table.string('sex')
-        table.date('dateOfBirth')
-        table.float('birth')
-        table.float('month1')
-        table.float('month2')
-        table.float('month4')
-        table.float('month6')
-        table.float('month9')
-        table.float('month12')
-        table.float('month15')
-        table.float('month18')
-        table.float('month24')
-    })
-  }
-  
-  exports.down = function(knex, Promise) {
-    return knex.schema.dropTableIfExists('female')
-  }
+  return knex.schema.createTable('female', table => {
+    table.increments().primary()
+    table.string('sex')
+    table.date('dateOfBirth')
+    table.float('birth')
+    table.float('month1')
+    table.float('month2')
+    table.float('month4')
+    table.float('month6')
+    table.float('month9')
+    table.float('month12')
+    table.float('month15')
+    table.float('month18')
+    table.float('month24')
+  })
+}
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('female')
+}
   
